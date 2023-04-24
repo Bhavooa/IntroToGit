ede909f827723ce47ea0ad0d2361481d9f8e5e79
Bhavya <bhavyarajput17@gmail.com>
2023-04-23 20:39:09 -0700

    Initial Commit

---
+++ IntroToGit/IntroToGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroToGitApp.swift
+//  IntroToGit
+//
+//  Created by Bhavya Rajput on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct IntroToGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
